45070b25ac0103a8ea69f0e109562dc5226196b6
sif1414 <ed14sif04@gmail.com>
2022-01-06 10:08:29 +0100

    CRUD operations

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -13,7 +13,6 @@ const pool = mysql.createPool({
 // show users
 exports.show = (req, res) => {
 
-
 pool.getConnection((err, connection) => {
   if (err) throw err;
   console.log("MySql connected");
@@ -31,4 +30,79 @@ pool.getConnection((err, connection) => {
 });
 
 
+};
+
+// find user
+exports.find = (req, res) => {
+
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log("MySql connected");
+
+    let searchUser = req.body.search;
+    connection.query(
+      "SELECT * FROM users WHERE first_name LIKE ? OR last_name LIKE ?",
+      ["%" + searchUser + "%", "%" + searchUser + "%"],
+      (err, databack) => {
+        connection.release();
+
+        if (!err) {
+          res.render("home", { databack });
+        } else {
+          console.log(err);
+        }
+        console.log("this is the data from users table: \n", databack);
+      }
+    );
+  });
+};
+
+//add user 
+// exports.adduserform = (req, res) => {
+//   const {Fname, Lname, Email, Phone, Comment} = req.body;
+//   pool.getConnection((err, connection) => {
+//     if (err) throw err;
+//     console.log("MySql connected");
+
+//     connection.query("INSERT INTO users SET first_name = ?, Lname = ?", [Fname, Lname],
+//       (err) => {
+//         connection.release();
+
+//         if (!err) {
+//           res.render("adduser");
+//         } else {
+//           console.log(err);
+//         }
+//         console.log("this is the data from users table: \n", databack);
+//       }
+//     );
+//   );
+//     };
+//   );
+// };
+//add user
+exports.adduserform = (req, res) => {
+  res.render("adduser");
+}
+
+exports.create = (req, res) => {
+  const { Fname, Lname, Email, Phone, Comment } = req.body;
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log("MySql connected");
+
+    connection.query(
+      "INSERT INTO users SET first_name = ?, last_name = ?, phone_number = ?, email = ?, comments = ?  ",
+      [Fname, Lname, Email, Phone, Comment],
+      (err) => {
+        connection.release();
+
+        if (!err) {
+          res.render("adduser", {alert: 'User added successfully.'});
+        } else {
+          console.log(err);
+        }
+      }
+    );
+  });
 };
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/usersController');
 
 router.get('/', userController.show);
+router.post('/', userController.find);
+router.get('/adduser', userController.adduserform);
+router.post('/adduser', userController.create);
 
 
 //Router
